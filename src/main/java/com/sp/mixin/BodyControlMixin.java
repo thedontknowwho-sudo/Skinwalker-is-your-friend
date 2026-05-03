711e9e2c4f3f7a92da6f0c5b9d98fd34522da92c
thedontknowwho-sudo <thedontknowwho@gmail.com>
2026-05-04 01:25:40 +0300

    Add files via upload

--- src/main/java/com/sp/mixin/BodyControlMixin.java
+++ src/main/java/com/sp/mixin/BodyControlMixin.java
@@ -1,1 +1,24 @@
+package com.sp.mixin;
 
+import com.sp.entity.custom.SkinWalkerEntity;
+import net.minecraft.entity.ai.control.BodyControl;
+import net.minecraft.entity.mob.MobEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BodyControl.class)
+public class BodyControlMixin {
+
+    @Shadow @Final private MobEntity entity;
+
+    @Inject(method = "slowlyAdjustBody", at = @At("HEAD"), cancellable = true)
+    private void redirect(CallbackInfo ci){
+        if(this.entity instanceof SkinWalkerEntity) {
+            ci.cancel();
+        }
+    }
+}
